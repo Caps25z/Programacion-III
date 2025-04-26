533239a6167f6a84ad64033be57802a1b2af2666
Caps25z <Meitno100130@gmail.com>
2025-04-26 12:03:19 -0500

    Update Conexion.java

--- Parcial Programacion Corte 2/TecnarDB/src/Clases/Conexion.java
+++ Parcial Programacion Corte 2/TecnarDB/src/Clases/Conexion.java
@@ -1,1 +1,22 @@
+package Clases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/TecnarDB", 
+                "root", 
+                "" // Tu contraseña de MySQL
+            );
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return con;
+    }
+}
